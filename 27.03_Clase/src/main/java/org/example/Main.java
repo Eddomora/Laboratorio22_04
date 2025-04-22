ec57d604c245ed18e5546bf6c4d09e7d4599d989
Eddo <eddomora@gmail.com>
2025-04-22 14:22:57 -0400

    creacion del ejemplo

---
+++ 27.03_Clase/src/main/java/org/example/Main.java
@@ -0,0 +1,51 @@
+// Clase de herencia, poliformismo e interfaces
+package org.example;
+
+//La flecha en UML es la extension de una clase. Una subclase, y superclase.
+//Subclase = se crea con "public class B extends A {}" con énfasis en el extends.
+//Puede usar métodos de su clase (B) y de la clase A
+
+//Superclase = clase donde se extiende de la subclase. Tiene métodos que la subclase ocupa
+
+// "this"= sirve para poder acceder a información del objeto actual. Intentar entender de mejor
+//forma el this
+
+// Super = se usa en superclase funciona como el this
+
+//protected se usa entre la superclase y la subclase.
+
+//Siempre hay una superclase
+
+//Si tenemos una superclase A y subclase B
+
+// en el main se inician por ejemplo= A objeto = new A();
+// o igual A objeto = new B(); que usando la subclase y la superclase
+
+//Para hacer polimorfismo, se debe tener all antes del nombre igual
+
+//Siguiente tema: CLASES ABSTRACTAS
+//Es una forma de poder llamar una clase, con las mismas formas pero sin cuerpo de código
+//solo el "título" de la clase. El cual se llama prototipo.
+
+//Las listas son más moldeables que los array
+
+//Siguiente tema= POLIMORFISMO DE CLASES
+
+//Si tenemos una superclase esta va a funcionar para instanciar,
+//no asi lo pueden hacer las subclases, las cuales solo pueden ser particulares.
+//Ej: Superclase objeto = new Super/Sub-clase
+
+//scanner sirve para poder obtener lo que se ingresa con el teclado en terminal
+
+//forma nueva de loop con for= for(Animales[] a : Animales){ sout(a); }
+//Ver "EjemploPolimorfismo" que tiene ejemplos de la clase.
+
+//Buscar duck typing
+
+//    Siguiente tema= INTERFACE
+//Es lo mismo que en c donde se hacía un archivo con toda la información de la funcion,
+//pero que en este caso se ocupa para poder usar "complements"
+
+//Ver que es default en java del ppt
+
+//
