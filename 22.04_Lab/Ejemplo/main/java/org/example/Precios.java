68603e6a2b684b2680e2c70e07bc85860a5d2612
Eddo <eddomora@gmail.com>
2025-04-29 13:46:48 -0400

    Actualizacion de los precios de la enumeracion

--- 22.04_Lab/Ejemplo/main/java/org/example/Precios.java
+++ 22.04_Lab/Ejemplo/main/java/org/example/Precios.java
@@ -3,7 +3,9 @@ package org.example;
 public enum Precios {
     COCACOLA(1500),
     SPRITE(1000),
-    DULCES(500);
+    FANTA(1100),
+    SUPER8(2000),
+    SNICKERS(2500);
     private final int precio;
     Precios(int precio) {
         this.precio = precio;
